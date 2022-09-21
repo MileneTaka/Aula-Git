9d9ce81472349a65c5bdb0acc94101d883b82aa7
Milene Takahashi <milenest87@gmail.com>
2022-09-20 21:07:08 -0300

    copy revisao

--- teste.js
+++ teste.js
@@ -1,3 +1,17 @@
 console.log("Milene")
 
 console.log("Takahashi")
+
+let carro = {
+    marca: "Fiat",
+    modelo: "Uno",
+ };
+ 
+ function Carro(valorMarca, valorModelo){
+    this.marca = valorMarca;
+    this.modelo = valorModelo;
+ }
+ 
+ console.log(carro);
+ 
+ console.log(new Carro("Renault", "Clio"));
